4a0df5fd5d6ad91c14f77668b4b0f5820c6ba3a7
Blake Neely <blakeneely21@gmail.com>
2019-10-20 17:33:43 -0500

    added friends sample array

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+// Make sample array of friends objects
+var friends = [
+    {
+        name: "Trent Reznor",
+        photo: "https://img.discogs.com/HUG1xiVk4NRgPKUmMZrzr9FXsg8=/600x559/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-27457-1283685575.jpeg.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Richard Hendricks",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/silicon-valley/characters/s1/richard-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+        scores: [
+            2,
+            5,
+            2,
+            2,
+            5,
+            3,
+            1,
+            1,
+            2,
+            4
+        ]
+    },
+    {
+        name: "Ryan Gosling",
+        photo: "https://i.pinimg.com/originals/1d/ea/1e/1dea1e32023b48752326e50e71ede2d5.jpg",
+        scores: [
+            1,
+            3,
+            3,
+            5,
+            2,
+            2,
+            5,
+            4,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Emma Stone",
+        photo: "https://i2-prod.mirror.co.uk/incoming/article4904699.ece/ALTERNATES/s615b/EmmaStone.jpg",
+        scores: [
+            3,
+            2,
+            2,
+            3,
+            1,
+            2,
+            3,
+            4,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Zooey Deschanel",
+        photo: "https://image.tmdb.org/t/p/original/ktjfylBzoJJik70F5VyGFiRgxUf.jpg",
+        scores: [
+            1,
+            5,
+            5,
+            1,
+            1,
+            3,
+            3,
+            5,
+            1,
+            4
+        ]
+    }
+];
+
+module.exports = friends;
