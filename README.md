e98775c0917677582b5f602b20b07a2c7b42bbcd
Blake Neely <blakeneely21@gmail.com>
2019-10-23 21:40:51 -0500

    commented out catch all route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# friendfinder
+# Friend Finder
+
+## Description
+    FriendFinder is a full stack, simple dating web app. Using Express.js for route handling the user completes a simple survey and receives back their most compatible match "friend" based on thier answers. You can take a look at the deployed live website on <a href="#">here</a> on Heroku.
+
+## What's inside
+    Express.js 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,10 +9,13 @@ module.exports = function(app){
     app.get("/", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
+
     // failsafe defaults any route not predefined
+
     // app.use(function(req,res){
     //     res.sendFile(path.join(__dirname + "/../public/home.html"));
     // });
+
     // app.get("*", function(req, res){
     //     res.sendFile(path.join(__dirname + "/../public/home.html"));
     // });
